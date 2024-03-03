ba59081ee7d593a94fb93539dbf81cd46fb5ef27
volry <v.v.ryzhuk@gmail.com>
2024-03-03 16:25:14 +0200

    working with slicer

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
-#%%
 import pandas as pd
 import streamlit as st
 
-#%%
 st.title("Stock Data Visualization 2")
 df = pd.read_excel(r"data/CIT_NN.xlsx", sheet_name='files')
-# %%
+
 df['datetime'] = pd.to_datetime(df['datetime'])
 
 # Get the minimum and maximum dates from your DataFrame
 min_date = df['datetime'].min()
 max_date = df['datetime'].max()
 
-# Use a slider to select the date range, using datetime objects directly
-start_date, end_date = st.slider(
-    'Select Date Range',
-    value=(min_date, max_date),
-    format='MM/DD/YYYY'
-)
+# Use two date_input widgets for start and end dates
+start_date = st.date_input('Start date', min_date)
+end_date = st.date_input('End date', max_date)
+
+# Ensure end_date is after start_date
+if start_date > end_date:
+    st.error('Error: End date must fall after start date.')
 
 # Filter the DataFrame based on the selected date range
-filtered_df = df[(df['datetime'] >= start_date) & (df['datetime'] <= end_date)]
+filtered_df = df[(df['datetime'] >= pd.to_datetime(start_date)) & (df['datetime'] <= pd.to_datetime(end_date))]
 
 # Display the filtered DataFrame
 st.dataframe(filtered_df)
