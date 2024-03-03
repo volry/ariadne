f8a10945b42ef41fc08d4da8ceb8c8a83be8edff
volry <v.v.ryzhuk@gmail.com>
2024-03-03 16:21:06 +0200

    fix date slicer

--- app.py
+++ app.py
@@ -3,23 +3,20 @@ import pandas as pd
 import streamlit as st
 
 #%%
-st.title("Stock Data Visualization")
+st.title("Stock Data Visualization 2")
 df = pd.read_excel(r"data/CIT_NN.xlsx", sheet_name='files')
 # %%
-##df.info()
-##st.dataframe(df)
+df['datetime'] = pd.to_datetime(df['datetime'])
 
 # Get the minimum and maximum dates from your DataFrame
 min_date = df['datetime'].min()
 max_date = df['datetime'].max()
 
-# Add a date range slider
+# Use a slider to select the date range, using datetime objects directly
 start_date, end_date = st.slider(
-    "Select Date Range",
-    min_value=min_date,
-    max_value=max_date,
+    'Select Date Range',
     value=(min_date, max_date),
-    format="MM/DD/YYYY"
+    format='MM/DD/YYYY'
 )
 
 # Filter the DataFrame based on the selected date range
