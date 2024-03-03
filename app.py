f517b86a81c3a8425be925f1f7353e4ed1714bd3
volry <v.v.ryzhuk@gmail.com>
2024-03-03 16:11:02 +0200

    adding date slicer

--- app.py
+++ app.py
@@ -7,4 +7,23 @@ st.title("Stock Data Visualization")
 df = pd.read_excel(r"data/CIT_NN.xlsx", sheet_name='files')
 # %%
 ##df.info()
-st.dataframe(df)
+##st.dataframe(df)
+
+# Get the minimum and maximum dates from your DataFrame
+min_date = df['datetime'].min()
+max_date = df['datetime'].max()
+
+# Add a date range slider
+start_date, end_date = st.slider(
+    "Select Date Range",
+    min_value=min_date,
+    max_value=max_date,
+    value=(min_date, max_date),
+    format="MM/DD/YYYY"
+)
+
+# Filter the DataFrame based on the selected date range
+filtered_df = df[(df['datetime'] >= start_date) & (df['datetime'] <= end_date)]
+
+# Display the filtered DataFrame
+st.dataframe(filtered_df)
