74f94a5b6a4b908c58c0bc3cda5306bf9bc4b81d
volry <v.v.ryzhuk@gmail.com>
2024-04-13 13:15:39 +0300

    fix df load error

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ if not ss.logged_in:
 
 # Main app content (only shown if logged in)
 if ss.logged_in:
-    df = load_data()
+    df = load_data_from_gcs()
     st.header('Indicators')
     # Place indicator checkboxes below the main chart
     show_enter = st.checkbox('Show Enter Predicted', True)
